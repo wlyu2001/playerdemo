81a091641aa45c2d418c4f7adbf48126f6d7258a
Lingyu Wang <wlyu2001@gmail.com>
2021-02-08 19:25:24 +0100

    first commit

---
+++ app/src/main/java/com/shishiapp/playerdemo/fragments/TokenFragment.kt
@@ -0,0 +1,28 @@
+package com.shishiapp.playerdemo.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import androidx.navigation.fragment.findNavController
+import com.shishiapp.playerdemo.R
+
+class TokenFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_token, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        view.findViewById<Button>(R.id.button_first).setOnClickListener {
+            findNavController().navigate(R.id.action_tokenFragment_to_sectionFragment)
+        }
+    }
+}
